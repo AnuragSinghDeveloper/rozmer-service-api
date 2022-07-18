0d6152354c1fc944210d6b7de77dd25599a51a62
=ashishdix04 <ashishdixo4@gmail.com>
2022-07-18 18:02:51 +0530

    Initial commit

---
+++ src/main/java/com/rozmer/service/RozmerServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.rozmer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozmerServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozmerServiceApiApplication.class, args);
+	}
+
+}
